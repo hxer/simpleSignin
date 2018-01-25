01945eed3684a0a6443c0d55ff30001134d54c1a
janes <haoxi4607@gmail.com>
2018-01-25 19:54:32 +0800

    just run unicomapp

--- ci-test.py
+++ ci-test.py
@@ -38,12 +38,6 @@ class TestSignin(unittest.TestCase):
         self.assertTrue(unicomapp.login(username, password))
         self.assertTrue(unicomapp.signin())
 
-    def test_jd(self):
-        username = os.getenv('jd_username') or JDong['username']
-        password = os.getenv('jd_password') or JDong['password']
-        jd =JD(username, password)
-        self.assertTrue(jd.sign())
-
 
 if __name__ == "__main__":
     unittest.main()
